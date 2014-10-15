64c8026b1ce118836dc19f845198cae458e919b2
dpolaristar <dpolaristar@gmail.com>
2014-10-15 13:41:52 -0500

    First version of Main. (To be updated once database and classes have been secured.)

---
+++ Library Catelog Program/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.ISBNTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.AuthorNameTextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TitleTextBox = New System.Windows.Forms.TextBox()
+        Me.CheckOutButton = New System.Windows.Forms.Button()
+        Me.ReturnButton = New System.Windows.Forms.Button()
+        Me.CheckOverDueAndFeesButton = New System.Windows.Forms.Button()
+        Me.CustomerDataGB = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SearchCustomerButton = New System.Windows.Forms.Button()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.CustomerDataGB.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(117, 12)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchButton.TabIndex = 0
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'ISBNTextBox
+        '
+        Me.ISBNTextBox.Location = New System.Drawing.Point(11, 25)
+        Me.ISBNTextBox.Name = "ISBNTextBox"
+        Me.ISBNTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.ISBNTextBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "ISBN:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Author:"
+        '
+        'AuthorNameTextBox
+        '
+        Me.AuthorNameTextBox.Location = New System.Drawing.Point(11, 67)
+        Me.AuthorNameTextBox.Name = "AuthorNameTextBox"
+        Me.AuthorNameTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.AuthorNameTextBox.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(27, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Title"
+        '
+        'TitleTextBox
+        '
+        Me.TitleTextBox.Location = New System.Drawing.Point(11, 106)
+        Me.TitleTextBox.Name = "TitleTextBox"
+        Me.TitleTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.TitleTextBox.TabIndex = 6
+        '
+        'CheckOutButton
+        '
+        Me.CheckOutButton.Location = New System.Drawing.Point(117, 41)
+        Me.CheckOutButton.Name = "CheckOutButton"
+        Me.CheckOutButton.Size = New System.Drawing.Size(75, 23)
+        Me.CheckOutButton.TabIndex = 7
+        Me.CheckOutButton.Text = "Check Out"
+        Me.CheckOutButton.UseVisualStyleBackColor = True
+        '
+        'ReturnButton
+        '
+        Me.ReturnButton.Location = New System.Drawing.Point(117, 70)
+        Me.ReturnButton.Name = "ReturnButton"
+        Me.ReturnButton.Size = New System.Drawing.Size(75, 23)
+        Me.ReturnButton.TabIndex = 8
+        Me.ReturnButton.Text = "Return"
+        Me.ReturnButton.UseVisualStyleBackColor = True
+        '
+        'CheckOverDueAndFeesButton
+        '
+        Me.CheckOverDueAndFeesButton.Location = New System.Drawing.Point(117, 99)
+        Me.CheckOverDueAndFeesButton.Name = "CheckOverDueAndFeesButton"
+        Me.CheckOverDueAndFeesButton.Size = New System.Drawing.Size(144, 23)
+        Me.CheckOverDueAndFeesButton.TabIndex = 9
+        Me.CheckOverDueAndFeesButton.Text = "Check Fees and Overdue"
+        Me.CheckOverDueAndFeesButton.UseVisualStyleBackColor = True
+        '
+        'CustomerDataGB
+        '
+        Me.CustomerDataGB.Controls.Add(Me.SearchCustomerButton)
+        Me.CustomerDataGB.Controls.Add(Me.Label7)
+        Me.CustomerDataGB.Controls.Add(Me.TextBox4)
+        Me.CustomerDataGB.Controls.Add(Me.Label6)
+        Me.CustomerDataGB.Controls.Add(Me.Label5)
+        Me.CustomerDataGB.Controls.Add(Me.Label4)
+        Me.CustomerDataGB.Controls.Add(Me.TextBox3)
+        Me.CustomerDataGB.Controls.Add(Me.TextBox2)
+        Me.CustomerDataGB.Controls.Add(Me.TextBox1)
+        Me.CustomerDataGB.Location = New System.Drawing.Point(11, 128)
+        Me.CustomerDataGB.Name = "CustomerDataGB"
+        Me.CustomerDataGB.Size = New System.Drawing.Size(261, 134)
+        Me.CustomerDataGB.TabIndex = 10
+        Me.CustomerDataGB.TabStop = False
+        Me.CustomerDataGB.Text = "Customer Data:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(161, 71)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(161, 110)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(3, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Customer ID:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(196, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Name:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(195, 94)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(48, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Address:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(161, 32)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(176, 55)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Phone number:"
+        '
+        'SearchCustomerButton
+        '
+        Me.SearchCustomerButton.Location = New System.Drawing.Point(4, 84)
+        Me.SearchCustomerButton.Name = "SearchCustomerButton"
+        Me.SearchCustomerButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchCustomerButton.TabIndex = 8
+        Me.SearchCustomerButton.Text = "Search"
+        Me.SearchCustomerButton.UseVisualStyleBackColor = True
+        '
+        'closeButton
+        '
+        Me.closeButton.Location = New System.Drawing.Point(198, 12)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(75, 23)
+        Me.closeButton.TabIndex = 11
+        Me.closeButton.Text = "Close"
+        Me.closeButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.closeButton)
+        Me.Controls.Add(Me.CustomerDataGB)
+        Me.Controls.Add(Me.CheckOverDueAndFeesButton)
+        Me.Controls.Add(Me.ReturnButton)
+        Me.Controls.Add(Me.CheckOutButton)
+        Me.Controls.Add(Me.TitleTextBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.AuthorNameTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ISBNTextBox)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Name = "Form1"
+        Me.Text = "Library Catelog"
+        Me.CustomerDataGB.ResumeLayout(False)
+        Me.CustomerDataGB.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SearchButton As System.Windows.Forms.Button
+    Friend WithEvents ISBNTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents AuthorNameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TitleTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents CheckOutButton As System.Windows.Forms.Button
+    Friend WithEvents ReturnButton As System.Windows.Forms.Button
+    Friend WithEvents CheckOverDueAndFeesButton As System.Windows.Forms.Button
+    Friend WithEvents CustomerDataGB As System.Windows.Forms.GroupBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents SearchCustomerButton As System.Windows.Forms.Button
+    Friend WithEvents closeButton As System.Windows.Forms.Button
+
+End Class
---
+++ Library Catelog Program/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub closeButton_Click(sender As System.Object, e As System.EventArgs) Handles closeButton.Click
+
+        Me.Close()
+
+    End Sub
+End Class
